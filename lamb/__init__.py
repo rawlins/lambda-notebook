b305e03b08d815847acbcb8eafe7f86b54485b58
Kyle Rawlins <rawlins@cogsci.jhu.edu>
2016-02-13 13:41:10 -0500

    update version for interim release

--- lamb/__init__.py
+++ lamb/__init__.py
@@ -2,5 +2,5 @@ __all__ = ['utils', 'types', 'meta', 'lang', 'tree_mini', 'parsing', 'magics',
 'lnsetup', 'display', 'combinators']
 
 __version_info__ = (0, 6, 2)
-__release__ = False
+__release__ = True
 __version__ = ".".join(str(i) for i in __version_info__)
