7d29c16cc6c09d7c87956ce0e511bee70753c461
Kyle Rawlins <rawlins@gmail.com>
2018-10-10 13:28:15 -0400

    Update version info for 0.6.5

--- lamb/__init__.py
+++ lamb/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['utils', 'types', 'meta', 'lang', 'tree_mini', 'parsing', 'magics',
 'lnsetup', 'display', 'combinators']
 
-__version_info__ = (0, 6, 3)
+__version_info__ = (0, 6, 5)
 __release__ = False
 __version__ = ".".join(str(i) for i in __version_info__)
