1a0c7cdb7bdee03cbbbe3176863f2d498acb45cd
advil <rawlins@gmail.com>
2021-10-05 17:33:23 -0400

    feat: more improvements to the in-repo install script

--- install_lambda_kernel.py
+++ install_lambda_kernel.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import sys, os, signal
+import sys, os, signal, argparse
 
 # utility for installing various versions of the lambda notebook kernel in
 # various places, for development purposes. Actual installation code is in
@@ -17,16 +17,34 @@ except ImportError:
 
 if __name__ == "__main__":
 	import lamb.lnsetup
-	lib_dir = None
-	suffix = ""
-	user = False
-	# TODO: actual cli handling
-	if len(sys.argv) > 1 and sys.argv[1] == "--force-path":
-		lib_dir = os.path.abspath(os.path.dirname(__file__))
+	parser = argparse.ArgumentParser(description=
+		"Install lambda notebook kernels. Use this script to install a kernel "
+		"when working with the lambda notebook version in repository, or to "
+		"test installations. Use `jupyter kernelspec list` to check what is "
+		"installed and where.")
+	parser.add_argument('--no-force-path', action='store_true',
+		help='Install a kernel that uses a system-installed `lamb` module. If not set (the default), the kernel forces use of the `lamb` module in the current directory.')
+	parser.add_argument('--dev', action='store_true',
+		help='Install a kernel named with `-dev`. Use this to have a kernel that co-exists with an system-installed one.')
+	parser.add_argument('--system', action='store_true',
+		help='Install the kernel in the system prefix directory, respecting environments; if not set (the default), use the per-user. If a user and system kernel with the same name are installed, the per-user version will take priority.\nThe current system prefix is: `%s`' % sys.prefix)
+	args = parser.parse_args()
+
+	if args.dev:
 		suffix = "-dev"
-		user = True
-	r = lamb.lnsetup.install_kernelspec(lib_dir=lib_dir, user=user, suffix=suffix)
-	result_msg = "Lambda notebook kernel installed in '%s'" % r
+	else:
+		suffix = ""
+
+	if args.no_force_path:
+		lib_dir = None
+	else:
+		# TODO: derive the path from where the `lamb` module is instead?
+		lib_dir = os.path.abspath(os.path.dirname(__file__))
+
+	r = lamb.lnsetup.install_kernelspec(lib_dir=lib_dir, user=not args.system, suffix=suffix)
+	result_msg = "Lambda notebook%s kernel installed in '%s'" % (suffix, r)
 	if lib_dir:
 		result_msg += ", using lamb module location '%s'" % lib_dir
 	print(result_msg)
+	if args.system and not r.startswith(sys.prefix):
+		print("Warning: system install of kernel is shadowed by a per-user lambda notebook kernel; use `jupyter kernelspec` to remove.")
