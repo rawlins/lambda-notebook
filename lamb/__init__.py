590d18585ac1cf88ef6c9fc26400277c538cfe8b
advil <rawlins@gmail.com>
2021-10-18 11:11:09 -0400

    build: clear release bit

--- lamb/__init__.py
+++ lamb/__init__.py
@@ -2,5 +2,5 @@ __all__ = ['utils', 'types', 'meta', 'lang', 'tree_mini', 'parsing', 'magics',
            'lnsetup', 'display', 'combinators']
 
 __version_info__ = (0, 6, 8)
-__release__ = True
+__release__ = False
 __version__ = ".".join(str(i) for i in __version_info__)
