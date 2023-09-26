rawlins/lambda-notebook

commit 9652ac0e91b2562d5ee875c249ccc377842e88b3

refactor: exercise more fine-grained control over reprs
The original approach (which is nearly as old as this project) to reprs
is pretty brute force: essentially everything is displayed via
_repr_html_ because in standard Jupyter, this works well with mathjax.
However, this causes a bunch of issues:
* In colab, _repr_html_ does not render latex
* at the command line, html code was displayed in a bunch of cases where
  it shouldn't be
* In some cases, text repr isn't available if the frontend wants that,
  even though the code was there
* In some cases _repr_html_ was used for pure latex output

This commit provides some scaffolding for finer-grained control over
what repr is used where, and fixes a bunch of cases. In some cases,
_repr_markdown_ is used, since in colab, this leads to latex rendering
and still allows some html (generally not styling, in my testing). Text
reprs are handled a little more cleanly for ipython output. This is all
still a little bit piecemeal, so standardizing some of this api could be
very helpful. It also doesn't deal with the biggest problem, i.e.
derivation trees.

lamb/lang.py
@@ -534,7 +534,8 @@ class SingletonComposable(Composable):
                 + self.content.latex_str()))
 
     def show(self):
-        return MiniLatex(self.latex_str())
+        # is using `latex` generally safe here?
+        return MiniLatex(latex=self.latex_str())
 
     def _repr_latex_(self):
         return self.latex_str()
@@ -1216,20 +1217,9 @@ class CompositionResult(Composable):
     def __repr__(self):
         return ("CompositionResult(results=%s, failures=%s)"
                                 % (repr(self.results), repr(self.failures)))
-        if len(self.results) == 0:
-            return repr(self.failures)
-        else:
-            return repr(self.result_items())
 
     def __str__(self):
-        s = str()
-        if (len(self.results) == 0):
-            s += "Composition failed:\n"
-            s += self.failures_str()
-        else:
-            for composite in self.results:
-                s += "    " + composite.compose_str()
-        return s
+        return self.summary(plain=True)
 
     @property
     def name(self):
@@ -1241,15 +1231,15 @@ class CompositionResult(Composable):
             else:
                 return str(self.source)
 
-    def show(self, recurse=True, style=None, failures=False):
-        s = str()
+    def summary(self, recurse=True, style=None, failures=False, plain=False):
+        s = ""
+        newline = plain and "\n" or "<br />\n"
         if (len(self.results) == 0):
             if self.source is None:
-                s += "Composition failed:<br />\n"
+                s += "Composition failed:"
             else:
-                s += ("Composition of %s failed:<br />\n"
-                                        % self.name)
-            s += self.failures_str_latex()
+                s += ("Composition of %s failed:" % (self.name))
+            s += newline + self.failures_str(latex = not plain)
         else:
             if (len(self.results) == 1):
                 s += "1 composition path.  Result:"
@@ -1257,22 +1247,35 @@ class CompositionResult(Composable):
                 s += "%i composition paths. Results:" % len(self.results)
             n = 0
             for composite in self.results:
-                #TODO: newlines in mathjax??
                 num = composite.collapsed_count
-                if num == 1:
-                    s += ("\n<br />" + latex_indent() +
-                                        "[%i]: " % n + composite.latex_str())
+                s += (newline + indent(latex = not plain) + "[%i]: " % n)
+                if plain:
+                    s += composite.compose_str()
                 else:
-                    s += ("\n<br />" +
-                            latex_indent() +
-                            ("[%i]: %s &nbsp;&nbsp;<span style=\"font-size:small\">(%i equivalent paths lead here)</span>"
-                                % (n, composite.latex_str(), num)))
+                    s += composite.latex_str()
+                if num > 1:
+                    if plain:
+                        s += " (%i equivalent paths lead here)" % num
+                    else:
+                        s += (" &nbsp;&nbsp;<span style=\"font-size:small\">(%i equivalent paths lead here)</span>"
+                                % num)
                 n += 1
             if failures:
-                s += ("\n<br /><br />" +
-                      "Composition attempts that failed:<br />\n" +
-                      self.failures_str_latex())
-        return MiniLatex(s)
+                s += (newline + newline
+                        + "Composition attempts that failed:" + newline
+                        + self.failures_str(latex = not plain))
+        return s
+
+    def show(self, recurse=True, style=None, failures=False):
+        return MiniLatex(markdown=self.summary(plain=False, recurse=recurse, style=style, failures=failures),
+            plain=self.summary(plain=True, recurse=recurse, style=style, failures=failures))
+
+    def _ipython_display_(self):
+        # note: currently this sidesteps implementing _repr_pretty_ by having
+        # the repr returned by show() be different than the object's repr...
+        # maybe not ideal?
+        import IPython
+        IPython.display.display(self.show())
 
     def build_summary_for_tree(self, style=None):
         defaultstyle = {"align": "left"}
@@ -1395,7 +1398,8 @@ class CompositionResult(Composable):
                     step_i += 1
                 sub_i += 1
             i += 1
-        return MiniLatex(s)
+        # TODO: set plain here
+        return MiniLatex(markdown=s)
 
     def tree(self, recurse=True, derivations=False, style=None):
         """Show the step-by-step derivation(s) as a proof tree."""
@@ -1417,7 +1421,8 @@ class CompositionResult(Composable):
             rst = r.tree(derivations=derivations, recurse=recurse, style=style)
             s += rst._repr_html_() + "<br /><br />"
             i += 1
-        return MiniLatex(s)
+        # TODO: set plain properly here...
+        return MiniLatex(html=s)
 
     def reduce_all(self):
         """Replace contents with versions that have been reduced as much as
@@ -1548,7 +1553,7 @@ class Items(CompositionResult):
                     s += ("%s &nbsp;&nbsp;<span style=\"font-size:small\">(%i equivalent items)</span>"
                                 % (composite.latex_str(i=n), num))
                 n += 1
-        return MiniLatex(s)
+        return MiniLatex(markdown=s)
 
     @property
     def name(self):

lamb/parsing.py
@@ -327,7 +327,7 @@ def html_output(accum, env):
         else:
             print("(Unknown class '%s') %s \\:=\\: %s" % (accum[k].__class__,
                                                           k, accum[k]))
-    return MiniLatex("<br />\n".join(lines), plain_s = "\n".join(plain_lines))
+    return MiniLatex(markdown="<br />\n".join(lines), plain="\n".join(plain_lines))
 
 def parse_qtree(s, i=0):
     s = s.strip()

lamb/utils.py
@@ -33,25 +33,35 @@ def get_tree_class():
         from lamb.tree_mini import Tree
         return Tree
 
+# TODO: this class' origins are much earlier in the ipython life cycle, can it
+# be modernized to align better with display classes? Or refactored away entirely?
+# if multiple args are specified, in Jupyter the priority is
+# markdown > html > latex > repr. Note that using html tags in markdown is
+# generally safe, but some frontends (*cough* colab) strip a whole bunch of
+# formatting. So it won't work for more complex things like derivation trees.
 class MiniLatex(object):
-    def __init__(self, latex_s, plain_s = None):
-        self.latex = latex_s
-        if plain_s is None:
-            self.plain = latex_s
-        else:
-            self.plain = plain_s
+    def __init__(self, html = None, latex = None, markdown = None, plain = ""):
+        self.latex = latex
+        self.html = html
+        self.markdown = markdown
+        if not plain:
+            plain = html or latex or markdown
+        self.plain = plain
 
     def __str__(self):
+        return self.plain
+
+    def __repr__(self):
         return self.plain
 
     def _repr_latex_(self):
         return self.latex
 
-    def __repr__(self):
-        return self.plain
-
     def _repr_html_(self):
-        return self.latex
+        return self.html
+
+    def _repr_markdown_(self):
+        return self.markdown
 
 def ltx_print(*args):
     s = ""

commit 3424d7a3d4f13af0d3511b5582b4a62d2ced55bd

fix: correctly handle `Items` in parsing output
Broken by 9652ac0e91b

lamb/lang.py
@@ -1550,10 +1550,20 @@ class Items(CompositionResult):
                 if num == 1:
                     s += composite.latex_str(i=n)
                 else:
+                    # should this really be here for Items?
                     s += ("%s &nbsp;&nbsp;<span style=\"font-size:small\">(%i equivalent items)</span>"
                                 % (composite.latex_str(i=n), num))
                 n += 1
         return utils.show(markdown=s)
+
+    def all_latex_strs(self):
+        # TODO: less ad hoc interface for this?
+        result = []
+        n = 0
+        for composite in self.results:
+            result.append(composite.latex_str(i=n))
+            n += 1
+        return result
 
     @property
     def name(self):

lamb/parsing.py
@@ -320,6 +320,9 @@ def html_output(accum, env):
                                     + "\\:=\\:"
                                     + accum[k]._repr_latex_()))
             plain_lines.append(repr(var) + " = " + repr(accum[k]))
+        elif isinstance(accum[k], lang.Items):
+            # TODO: less ad hoc treatment of this case
+            lines.extend(accum[k].all_latex_strs())
         elif isinstance(accum[k], lang.Composable):
             # item will automatically print an equality statement
             lines.append(accum[k]._repr_latex_())

notebooks/documentation/Lexical ambiguity.ipynb
@@ -61,6 +61,15 @@
    ]
   },
   {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "(the * bank[1])"
+   ]
+  },
+  {
    "cell_type": "markdown",
    "metadata": {},
    "source": [
@@ -218,7 +227,7 @@
    "name": "python",
    "nbconvert_exporter": "python",
    "pygments_lexer": "ipython3",
-   "version": "3.10.5"
+   "version": "3.9.7"
   }
  },
  "nbformat": 4,