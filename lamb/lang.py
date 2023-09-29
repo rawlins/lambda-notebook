rawlins/lambda-notebook

commit d2b5f188f4ac3c30761a5850de11a08a9dfc69c2

feat: prototype colab html+latex rendering
This commit (finally) accomplishes working rendering for the parts of
lambda notebook that embed latex math code within a complex html tree.
See the comment added in this commit for details, but the challenge is
that colab renders outputs in an iframe and uses katex (not mathjax) in
a fairly idiosyncratic way when doing so. It doesn't call katex at all
on HTML outputs, markdown outputs strip html attributes, and HTMLMath
widgets simply don't work; so I had a challenge figuring out how to get
any sort of rendering at all to happen for these complex outputs. This
commit injects katex autorender to lambda notebook HTML outputs, and
calls it.

colab issue: https://github.com/googlecolab/colabtools/issues/3941

I also took this opportunity to rename MiniLatex, since it's no longer
very descriptive of what it does.

lamb/lang.py
@@ -2,7 +2,7 @@
 import collections, itertools, logging, html
 
 from lamb import utils, types, meta, display
-from lamb.utils import ensuremath, MiniLatex
+from lamb.utils import ensuremath
 from lamb.types import type_e, type_t, type_property, TypeMismatch
 from lamb.meta import  TypedExpr
 from lamb import tree_mini
@@ -535,7 +535,7 @@ class SingletonComposable(Composable):
 
     def show(self):
         # is using `latex` generally safe here?
-        return MiniLatex(latex=self.latex_str())
+        return utils.show(latex=self.latex_str())
 
     def _repr_latex_(self):
         return self.latex_str()
@@ -1267,7 +1267,7 @@ class CompositionResult(Composable):
         return s
 
     def show(self, recurse=True, style=None, failures=False):
-        return MiniLatex(markdown=self.summary(plain=False, recurse=recurse, style=style, failures=failures),
+        return utils.show(markdown=self.summary(plain=False, recurse=recurse, style=style, failures=failures),
             plain=self.summary(plain=True, recurse=recurse, style=style, failures=failures))
 
     def _ipython_display_(self):
@@ -1399,7 +1399,7 @@ class CompositionResult(Composable):
                 sub_i += 1
             i += 1
         # TODO: set plain here
-        return MiniLatex(markdown=s)
+        return utils.show(markdown=s)
 
     def tree(self, recurse=True, derivations=False, style=None):
         """Show the step-by-step derivation(s) as a proof tree."""
@@ -1417,12 +1417,12 @@ class CompositionResult(Composable):
                 if r.collapsed_count > 1:
                     s += "(%i other equivalent paths)" % (r.collapsed_count - 1)
                 s += "<br />\n" 
-            # this will return a MiniLatex-packaged string.
+            # this will return a LNDisplay-packaged string.
             rst = r.tree(derivations=derivations, recurse=recurse, style=style)
             s += rst._repr_html_() + "<br /><br />"
             i += 1
         # TODO: set plain properly here...
-        return MiniLatex(html=s)
+        return utils.show(html=s)
 
     def reduce_all(self):
         """Replace contents with versions that have been reduced as much as
@@ -1553,7 +1553,7 @@ class Items(CompositionResult):
                     s += ("%s &nbsp;&nbsp;<span style=\"font-size:small\">(%i equivalent items)</span>"
                                 % (composite.latex_str(i=n), num))
                 n += 1
-        return MiniLatex(markdown=s)
+        return utils.show(markdown=s)
 
     @property
     def name(self):

lamb/parsing.py
@@ -1,6 +1,6 @@
 import sys, re, traceback, collections
 from lamb import utils
-from lamb.utils import *
+from lamb.utils import * # TODO: remove
 
 Tree = utils.get_tree_class()
 
@@ -327,7 +327,7 @@ def html_output(accum, env):
         else:
             print("(Unknown class '%s') %s \\:=\\: %s" % (accum[k].__class__,
                                                           k, accum[k]))
-    return MiniLatex(markdown="<br />\n".join(lines), plain="\n".join(plain_lines))
+    return utils.show(markdown="<br />\n".join(lines), plain="\n".join(plain_lines))
 
 def parse_qtree(s, i=0):
     s = s.strip()

lamb/lnsetup.py
@@ -20,12 +20,21 @@ except:
 
 KERNEL_NAME = "lambda-notebook"
 
+def colab_setup():
+    # check if we are running in colab. This won't necessarily work outside
+    # of an ipython context.
+    if 'google.colab' not in sys.modules:
+        return
+    # activate colab-specific display code
+    lamb.utils.LNDisplay = lamb.utils.ColabLNDisplay
+
 def inject_into_ipython():
     try:
         ip = get_ipython()
         # inject the module names
         ip.user_ns["lamb"] = lamb
         ip.user_ns["utils"] = lamb.utils
+        colab_setup()
         ip.user_ns["types"] = lamb.types
         ip.user_ns["meta"] = lamb.meta
         ip.user_ns["lang"] = lamb.lang
@@ -34,7 +43,6 @@ def inject_into_ipython():
         # inject some convenience functions
         ip.user_ns["reload_lamb"] = reload_lamb
         ip.user_ns["Tree"] = lamb.utils.get_tree_class()
-        ip.user_ns["MiniLatex"] = lamb.utils.MiniLatex
         ip.user_ns["ltx_print"] = lamb.utils.ltx_print
         ip.user_ns["te"] = lamb.meta.te
         ip.user_ns["tp"] = lamb.meta.tp
@@ -51,6 +59,7 @@ def reload_lamb(use_nltk_tree=None):
     # should this reload the magics?
     import importlib
     importlib.reload(lamb.utils)
+    colab_setup()
     if use_nltk_tree is not None:
         # inherit default from currently running version. TODO: too confusing?
         lamb.utils.use_nltk = use_nltk_tree

lamb/utils.py
@@ -33,13 +33,23 @@ def get_tree_class():
         from lamb.tree_mini import Tree
         return Tree
 
+# Katex autorender code, for use in a colab (Google Colaboratory) iframe.
+# this is a tweaked version of https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/contrib/auto-render.min.js
+# * enable $ as a delimiter (colab does this already, but katex doesn't by default)
+# * add on a render call
+# * difference from colab: leave \(\) enabled as delimiters. (Or, maybe these are enabled in colab, but are buggy?)
+# In principle, possibly this could be pulled dynamically from a CDN, but it's
+# easier / smoother to hardcode it and avoid external dependencies.
+katex_autorender_min = (r'''!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t(require("katex")):"function"==typeof define&&define.amd?define(["katex"],t):"object"==typeof exports?exports.renderMathInElement=t(require("katex")):e.renderMathInElement=t(e.katex)}("undefined"!=typeof self?self:this,(function(e){return function(){"use strict";var t={771:function(t){t.exports=e}},r={};function n(e){var i=r[e];if(void 0!==i)return i.exports;var a=r[e]={exports:{}};return t[e](a,a.exports,n),a.exports}n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,{a:t}),t},n.d=function(e,t){for(var r in t)n.o(t,r)&&!n.o(e,r)&&Object.defineProperty(e,r,{enumerable:!0,get:t[r]})},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)};var i={};return function(){n.d(i,{default:function(){return s}});var e=n(771),t=n.n(e),r=function(e,t,r){for(var n=r,i=0,a=e.length;n<t.length;){var o=t[n];if(i<=0&&t.slice(n,n+a)===e)return n;"\\"===o?n++:"{"===o?i++:"}"===o&&i--,n++}return-1},a=/^\\begin{/,o=function(e,t){for(var n,i=[],o=new RegExp("("+t.map((function(e){return e.left.replace(/[-/\\^$*+?.()|[\]{}]/g,"\\$&")})).join("|")+")");-1!==(n=e.search(o));){n>0&&(i.push({type:"text",data:e.slice(0,n)}),e=e.slice(n));var l=t.findIndex((function(t){return e.startsWith(t.left)}));if(-1===(n=r(t[l].right,e,t[l].left.length)))break;var d=e.slice(0,n+t[l].right.length),s=a.test(d)?d:e.slice(t[l].left.length,n);i.push({type:"math",data:s,rawData:d,display:t[l].display}),e=e.slice(n+t[l].right.length)}return""!==e&&i.push({type:"text",data:e}),i},l=function(e,r){var n=o(e,r.delimiters);if(1===n.length&&"text"===n[0].type)return null;for(var i=document.createDocumentFragment(),a=0;a<n.length;a++)if("text"===n[a].type)i.appendChild(document.createTextNode(n[a].data));else{var l=document.createElement("span"),d=n[a].data;r.displayMode=n[a].display;try{r.preProcess&&(d=r.preProcess(d)),t().render(d,l,r)}catch(e){if(!(e instanceof t().ParseError))throw e;r.errorCallback("KaTeX auto-render: Failed to parse `"+n[a].data+"` with ",e),i.appendChild(document.createTextNode(n[a].rawData));continue}i.appendChild(l)}return i},d=function e(t,r){for(var n=0;n<t.childNodes.length;n++){var i=t.childNodes[n];if(3===i.nodeType){for(var a=i.textContent,o=i.nextSibling,d=0;o&&o.nodeType===Node.TEXT_NODE;)a+=o.textContent,o=o.nextSibling,d++;var s=l(a,r);if(s){for(var f=0;f<d;f++)i.nextSibling.remove();n+=s.childNodes.length-1,t.replaceChild(s,i)}else n+=d}else 1===i.nodeType&&function(){var t=" "+i.className+" ";-1===r.ignoredTags.indexOf(i.nodeName.toLowerCase())&&r.ignoredClasses.every((function(e){return-1===t.indexOf(" "+e+" ")}))&&e(i,r)}()}},s=function(e,t){if(!e)throw new Error("No element provided to render");var r={};for(var n in t)t.hasOwnProperty(n)&&(r[n]=t[n]);r.delimiters=r.delimiters||[{left:"$$",right:"$$",display:!0},{left:"\\(",right:"\\)",display:!1},{left:"$",right:"$",display:!1},{left:"\\begin{equation}",right:"\\end{equation}",display:!0},{left:"\\begin{align}",right:"\\end{align}",display:!0},{left:"\\begin{alignat}",right:"\\end{alignat}",display:!0},{left:"\\begin{gather}",right:"\\end{gather}",display:!0},{left:"\\begin{CD}",right:"\\end{CD}",display:!0},{left:"\\[",right:"\\]",display:!0}],r.ignoredTags=r.ignoredTags||["script","noscript","style","textarea","pre","code","option"],r.ignoredClasses=r.ignoredClasses||[],r.errorCallback=r.errorCallback||console.error,r.macros=r.macros||{},d(e,r)}}(),i=i.default}()}));'''
+    + "renderMathInElement(document.body);")
+
 # TODO: this class' origins are much earlier in the ipython life cycle, can it
 # be modernized to align better with display classes? Or refactored away entirely?
 # if multiple args are specified, in Jupyter the priority is
 # markdown > html > latex > repr. Note that using html tags in markdown is
 # generally safe, but some frontends (*cough* colab) strip a whole bunch of
 # formatting. So it won't work for more complex things like derivation trees.
-class MiniLatex(object):
+class BaseLNDisplay(object):
     def __init__(self, html = None, latex = None, markdown = None, plain = ""):
         self.latex = latex
         self.html = html
@@ -63,6 +73,57 @@ class MiniLatex(object):
     def _repr_markdown_(self):
         return self.markdown
 
+# this class is only suitable for colab; it requires on colab's idiosyncratic
+# combination of iframes and katex rendering for outputs.
+class ColabLNDisplay(BaseLNDisplay):
+    def __init__(self, html = None, latex = None, markdown = None, plain = ""):
+        super().__init__(html, latex, markdown, plain)
+
+    def _ipython_display_(self):
+        import IPython.display
+        # colab does not render latex in HTML outputs, so we do a complicated
+        # maneuver to trigger it manually. It took me *many* tries to find
+        # something that works for this purpose. Some notes:
+        # * Issue: https://github.com/googlecolab/colabtools/issues/3941
+        # * Colab markdown (at least as of mid-2023) does render latex, but
+        #   strips html attributes, making it impossible to use for complex
+        #   outputs
+        # * Every colab output is rendered in its own iframe, "for security",
+        #   drastically complicating everything. It's possible to load mathjax
+        #   in an iframe, but this is heavy/slow, and not entirely reliable
+        #   anyways
+        # * within an iframe, colab uses katex (rather than mathjax), for speed.
+        #   It injects a minified version of katex, rather than loading it
+        #   externally. Calling katex (as far as I can tell) is fairly
+        #   integrated with minified parsing code, and I couldn't figure out a
+        #   way to trigger colab's katex render code directly. It doesn't load
+        #   or call katex autorender.
+        # * colab supports ipywidgets, but HTMLMath in particular is broken on
+        #   colab (see https://github.com/googlecolab/colabtools/issues/2680)
+        #
+        # Solution: ensure that colab injects its katex setup, display the
+        # HTML output, then inject and call a version of katex autorender.
+        if self.html and not self.markdown:
+            # This initial dummy call causes colab to inject its katex code into
+            # the iframe
+            IPython.display.display(IPython.display.Latex(""))
+            # now display the actual output
+            IPython.display.display(IPython.display.HTML(self.html))
+            # finally, inject (and run) the autorender code. This will produce
+            # js errors if there is no katex present in the document context!
+            IPython.display.display(IPython.display.Javascript(katex_autorender_min))
+        else:
+            # should work for everything else...
+            IPython.display.display(super())
+
+# warning! The value of LNDisplay may change, so it is not recommended to do
+# from utils import LNDisplay. Use `show` below instead.
+LNDisplay = BaseLNDisplay
+
+def show(**args):
+    return LNDisplay(**args)
+
+# TODO: remove, it's only used in a very old demo and a few misc places
 def ltx_print(*args):
     s = ""
     for x in args:
@@ -77,7 +138,7 @@ def ltx_print(*args):
                 else:
                     s += html.escape(repr(x))
         s += "<br />"
-    return MiniLatex(s)
+    return show(s)
 
 # from AIMA utils
 def num_or_str(x, allow_float=False):

commit 242531e7efffbebad5d1c3e2a852a4e12c4874d0

fix: rendering of Lexicon objects
This was broken by d2b5f188f4ac. Unfortunately, exceptions in repr code
are swallowed by IPython (the stack trace is printed, that's all), so my
automated testing missed this. Possibly the repr code could use some
explicit unit tests because of this.

lamb/lang.py
@@ -458,17 +458,12 @@ class Lexicon(MutableMapping):
     def reset(self):
         self.items = collections.OrderedDict()
 
-    def _repr_html_(self):
-        if len(self) == 0:
-            return "<i>(Empty lexicon)</i>"
-        lines = list()
-        for k in self:
-            if isinstance(self[k], Composable):
-                lines.append(self[k]._repr_html_())
-            else:
-                print("(Unknown class '%s') %s \\:=\\: %s" % (self[k].__class__,
-                                                          k, self[k]))
-        return "<br />\n".join(lines)
+    def _repr_markdown_(self):
+        # assumption: parsing output generates markdown code. TODO: unify
+        # these two things in a more elegant way. Probably Lexicon should be
+        # responsible for rendering?
+        from lamb import parsing
+        return parsing.html_output(self, dict())._repr_markdown_()
 
 
 class SingletonComposable(Composable):

lamb/parsing.py
@@ -313,6 +313,9 @@ def html_output(accum, env):
     from lamb import meta, lang
     lines = []
     plain_lines = []
+    if len(accum) == 0:
+        # use markdown for consistency...
+        return utils.show(markdown="<i>(Empty lexicon)</i>")
     for k in accum.keys():
         if isinstance(accum[k], meta.TypedExpr):
             var = fullvar(accum, k)