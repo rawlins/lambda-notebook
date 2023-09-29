21c11c777d4eda6a5b7314fdf702faea369e7849
advil <rawlins@gmail.com>
2023-09-29 08:25:44 -0400

    build: increment version

--- lamb/__init__.py
+++ lamb/__init__.py
@@ -1,8 +1,8 @@
 __all__ = ['utils', 'types', 'meta', 'lang', 'tree_mini', 'parsing', 'magics',
            'lnsetup', 'display', 'combinators', 'auto']
 
-__version_info__ = (0, 7, 0)
-__release__ = True
+__version_info__ = (0, 7, 1)
+__release__ = False
 __version__ = ".".join(str(i) for i in __version_info__)
 if not __release__:
     __version__ = __version__ + "a1"
