7c78114be9d0a158833cbded83d413b322453d7d
Kyle Rawlins <rawlins@gmail.com>
2025-11-11 16:08:27 -0500

    chore: update version and changelog for release

--- lamb/__init__.py
+++ lamb/__init__.py
@@ -1,8 +1,8 @@
 __all__ = ['utils', 'types', 'meta', 'lang', 'tree_mini', 'parsing', 'magics',
            'lnsetup', 'display', 'combinators', 'auto', 'console']
 
-__version_info__ = (0, 7, 1)
-__release__ = False
+__version_info__ = (0, 8, 0)
+__release__ = True
 __version__ = ".".join(str(i) for i in __version_info__)
 if not __release__:
     __version__ = __version__ + "a1"
