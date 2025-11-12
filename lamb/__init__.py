ffda3b0fad574e6b4c1798da0651a5549c192e5a
advil <rawlins@gmail.com>
2025-11-12 16:42:14 -0500

    chore: increment version tag

--- lamb/__init__.py
+++ lamb/__init__.py
@@ -1,8 +1,8 @@
 __all__ = ['utils', 'types', 'meta', 'lang', 'tree_mini', 'parsing', 'magics',
            'lnsetup', 'display', 'combinators', 'auto', 'console']
 
-__version_info__ = (0, 8, 0)
-__release__ = True
+__version_info__ = (0, 9, 0)
+__release__ = False
 __version__ = ".".join(str(i) for i in __version_info__)
 if not __release__:
     __version__ = __version__ + "a1"
